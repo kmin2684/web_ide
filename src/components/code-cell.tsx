e0c2435b17b82dd3f65c8d487c102f449eedc60f
kmin2684 <65061813+kmin2684@users.noreply.github.com>
2022-06-26 15:52:55 -0600

    inital code displayed is the current time

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,21 +1,49 @@
-import { useState } from 'react';
-import CodeEditor from './code-editor';
-import Preview from './preview';
-import bundle from '../bundler';
+import { useState, useEffect } from "react";
+import CodeEditor from "./code-editor";
+import Preview from "./preview";
+import bundle from "../bundler";
+
+const initialCode = `
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+class Clock extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+function tick() {
+  root.render(<Clock date={new Date()} />);
+}
+
+setInterval(tick, 1000);
+`;
 
 const CodeCell = () => {
-  const [code, setCode] = useState('');
-  const [input, setInput] = useState('');
+  const [code, setCode] = useState("");
+  const [input, setInput] = useState(initialCode);
 
   const onClick = async () => {
     const output = await bundle(input);
     setCode(output);
   };
 
+  useEffect(() => {
+    onClick();
+  }, []);
+
   return (
     <div>
       <CodeEditor
-        initialValue="const a = 1;"
+        initialValue={initialCode}
         onChange={(value) => setInput(value)}
       />
       <div>
