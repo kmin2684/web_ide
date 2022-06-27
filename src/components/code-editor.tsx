026eff88f7a676a32207e943070484e8eb0271c9
kmin2684 <65061813+kmin2684@users.noreply.github.com>
2022-06-27 02:21:15 -0600

    still working on monaco jsx highlighter

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -4,11 +4,12 @@ import Editor, { OnMount, OnChange } from "@monaco-editor/react";
 import prettier from "prettier";
 import parser from "prettier/parser-babel";
 
-// import * as Monaco from "monaco-editor";
-// import * as Monaco from "monaco-editor/esm/vs/editor/editor.api";
-// import { parse } from "@babel/parser";
-// import traverse from "@babel/traverse";
-// import MonacoJSXHighlighter from "monaco-jsx-highlighter";
+window.Buffer = window.Buffer || require("buffer").Buffer;
+window.process = {
+  ...window.process,
+};
+
+console.log("process\n", window.process);
 
 interface CodeEditorProps {
   initialValue: string;
@@ -18,13 +19,15 @@ interface CodeEditorProps {
 // fancy dynamic loader attached to the @monaco-editor/react's onMount callback
 const activateMonacoJSXHighlighter = async (monacoEditor: any, monaco: any) => {
   // monaco-jsx-highlighter depends on these in addition to Monaco and an instance of a Monaco Editor.
+
   const { default: traverse } = await import("@babel/traverse");
+  // // console.log(traverse);
+
   const { parse } = await import("@babel/parser");
   // >>> The star of the show =P >>>
   const { default: MonacoJSXHighlighter, JSXTypes } = await import(
     "monaco-jsx-highlighter" // Note: there is a polyfilled version alongside the regular version.
   ); // For example, starting with 2.0.2, 2.0.2-polyfilled is also available.
-
   // Instantiate the highlighter
   const monacoJSXHighlighter = new MonacoJSXHighlighter(
     monaco, // references Range and other APIs
@@ -32,73 +35,80 @@ const activateMonacoJSXHighlighter = async (monacoEditor: any, monaco: any) => {
     traverse, // helps collecting the JSX expressions within the AST
     monacoEditor // highlights the content of that editor via decorations
   );
+  console.log("monacoJSXHighlighter\n", monacoJSXHighlighter);
   // Start the JSX highlighting and get the dispose function
-  let disposeJSXHighlighting =
-    monacoJSXHighlighter.highlightOnDidChangeModelContent();
+  // let disposeJSXHighlighting =
+  monacoJSXHighlighter.highLightOnDidChangeModelContent();
   // Enhance monaco's editor.action.commentLine with JSX commenting and get its disposer
-  let disposeJSXCommenting = monacoJSXHighlighter.addJSXCommentCommand();
+  // let disposeJSXCommenting = monacoJSXHighlighter.addJSXCommentCommand();
+  monacoJSXHighlighter.addJSXCommentCommand();
   // <<< You are all set. >>>
 
-  // Optional: customize the color font in JSX texts (style class JSXElement.JSXText.tastyPizza from ./index.css)
-  JSXTypes.JSXText.options.inlineClassName = "JSXElement.JSXText.tastyPizza";
-  // more details here: https://microsoft.github.io/monaco-editor/api/interfaces/monaco.editor.IModelDecorationOptions.html
-  console.log(
-    "Customize each JSX expression type's options, they must match monaco.editor.IModelDecorationOptions:",
-    JSXTypes
-  );
+  console.log("highlighter is activated");
+  // // Optional: customize the color font in JSX texts (style class JSXElement.JSXText.tastyPizza from ./index.css)
+  // JSXTypes.JSXText.options.inlineClassName = "JSXElement.JSXText.tastyPizza";
+  // // more details here: https://microsoft.github.io/monaco-editor/api/interfaces/monaco.editor.IModelDecorationOptions.html
+  // console.log(
+  //   "Customize each JSX expression type's options, they must match monaco.editor.IModelDecorationOptions:",
+  //   JSXTypes
+  // );
 
-  // This example's shorthands for toggling actions
-  const toggleJSXHighlighting = () => {
-    if (disposeJSXHighlighting) {
-      disposeJSXHighlighting();
-      disposeJSXHighlighting = null;
-      return false;
-    }
+  // // This example's shorthands for toggling actions
+  // const toggleJSXHighlighting = () => {
+  //   if (disposeJSXHighlighting) {
+  //     disposeJSXHighlighting();
+  //     disposeJSXHighlighting = null;
+  //     return false;
+  //   }
 
-    disposeJSXHighlighting =
-      monacoJSXHighlighter.highlightOnDidChangeModelContent();
-    return true;
-  };
+  //   disposeJSXHighlighting =
+  //     monacoJSXHighlighter.highlightOnDidChangeModelContent();
+  //   return true;
+  // };
 
-  const toggleJSXCommenting = () => {
-    if (disposeJSXCommenting) {
-      disposeJSXCommenting();
-      disposeJSXCommenting = null;
-      return false;
-    }
+  // const toggleJSXCommenting = () => {
+  //   if (disposeJSXCommenting) {
+  //     disposeJSXCommenting();
+  //     disposeJSXCommenting = null;
+  //     return false;
+  //   }
 
-    disposeJSXCommenting = monacoJSXHighlighter.addJSXCommentCommand();
-    return true;
-  };
+  //   disposeJSXCommenting = monacoJSXHighlighter.addJSXCommentCommand();
+  //   return true;
+  // };
 
-  const isToggleJSXHighlightingOn = () => !!disposeJSXHighlighting;
-  const isToggleJSXCommentingOn = () => !!disposeJSXCommenting;
+  // const isToggleJSXHighlightingOn = () => !!disposeJSXHighlighting;
+  // const isToggleJSXCommentingOn = () => !!disposeJSXCommenting;
 
-  return {
-    monacoJSXHighlighter,
-    toggleJSXHighlighting,
-    toggleJSXCommenting,
-    isToggleJSXHighlightingOn,
-    isToggleJSXCommentingOn,
-  };
+  // return {
+  //   monacoJSXHighlighter,
+  //   toggleJSXHighlighting,
+  //   toggleJSXCommenting,
+  //   isToggleJSXHighlightingOn,
+  //   isToggleJSXCommentingOn,
+  // };
 };
 
 const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
   const editorRef = useRef<any>();
+  const monacoJSXHighlighterRef = useRef<any>(null);
   const [isEditorReady, setIsEditorReady] = useState(false);
   const [isJSXHighlightingOn, setIsJSXHighlightingOn] = useState(false);
-  let monacoJSXHighlighter;
-  // const onEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
-  //   editorRef.current = monacoEditor;
-  //   monacoEditor.onDidChangeModelContent(() => {
-  //     onChange(getValue());
-  //   });
 
-  //   monacoEditor.getModel()?.updateOptions({ tabSize: 2 });
-  // };
-
-  const handleEditorDidMount: OnMount = useCallback((editor, monaco) => {
-    editorRef.current = editor;
+  const handleEditorDidMount: OnMount = useCallback((monacoEditor, monaco) => {
+    editorRef.current = monacoEditor;
+    activateMonacoJSXHighlighter(monacoEditor, monaco)
+      .then((monacoJSXHighlighterRefCurrent) => {
+        monacoJSXHighlighterRef.current = monacoJSXHighlighterRefCurrent;
+        // setIsEditorReady(!!monacoEditor);
+        // setIsJSXHighlightingOn(
+        //   monacoJSXHighlighterRefCurrent.isToggleJSXHighlightingOn()
+        // );
+        // setIsJSXCommentingOn(
+        //   monacoJSXHighlighterRefCurrent.isToggleJSXCommentingOn()
+        // );
+      })
+      .catch((e) => console.log(e));
   }, []);
 
   const handleEditorDidChange: OnChange = (value, event) => {
